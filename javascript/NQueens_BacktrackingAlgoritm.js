8d2e8a4b2c872c963b9c8bc0435a7dd41dd246e6
DSSzymanski <dszymanski@umass.edu>
2021-03-02 09:08:39 -0500

    added documenting for Backtracking Algorithm js file and some other minor comments

--- javascript/NQueens_BacktrackingAlgoritm.js
+++ javascript/NQueens_BacktrackingAlgoritm.js
@@ -1,22 +1,47 @@
+/*
+*solve() is the main function within this module. Implements a recursive backtracking
+*	algorithm to solve for the current board.
+*@param {canvas node} board: canvas node relating to the chess board. used as a param
+*	for the drawing functions
+*@param {int} size: size of the board. Used as a param within the drawing functions as
+*	well as checking if the board is completely solved and as a bound for the for loops
+*	that check all rows for possible queen tiles.
+*@param {array} arr: array used for storing current queen locations for the recursive
+*	function calls. First call to the solve function can just take an empty array.
+*@returns bool: Returns boolean value for if the board size has a solution or not.
+*	True represents the board size being solved, false represents no soluion.
+*/
 function solve(board, size, arr) {
+	//Check if board puzzle is solved
 	if (arr.length === size) {
-		console.log(arr);
 		return true;
 	}
+	//for loop to iterate through the possible rows on the board
 	for (var row = 0; row < size; row += 1) {
+		//check if position is possible, if so add to array and recursively call function.
 		if (check_constraints(arr, row) === true) {
 			arr.push(row);
 			draw_canvas(board, size, arr);
+			//recursive call will return true if expansion on arr works, false otherwise.
 			if (solve(board, size, arr) === true) {
 				return true;
 			}
+			//remove node from arr and redraw canvas to reflect removing.
 			arr.pop();
 			draw_canvas(board, size, arr);
 		}
 	}
+	//returns false if no row works to find a solution
 	return false;
 }
 
+/*
+*check_constraints() is a general function that calls functions to check every constraint.
+*@param {array} arr: array of currently valid queen locations.
+*@param {int} row: location of next location to check.
+*@returns {boolean}: returns boolean value of true if the location passed all constraints,
+*	false otherwise.
+*/
 function check_constraints(arr, row) {
 	if (check_row(arr, row) === false) {
 		return false;
@@ -30,8 +55,16 @@ function check_constraints(arr, row) {
 	return true;
 }
 
+/*
+*check_upper_diag() checks if the queen attacks any queen in the upper left diagonal position.
+*@param {array} arr: array of currently valid queen locations.
+*@param {int} row: location of next location to check.
+*@returns {boolean}: returns boolean value of true if the location passed false otherwise.
+*/
 function check_upper_diag(arr, row) {
+	//decrement row to not check current position (can't attack itself).
 	row -= 1;
+	//loop to move to the left until you hit the left side of the board.
 	for (var i = arr.length-1; i >= 0; i -=1) {
 		if (arr[i] === row) {return false;}
 		row -= 1; 
@@ -39,8 +72,16 @@ function check_upper_diag(arr, row) {
 	return true;
 }
 
+/*
+*check_lower_diag() checks if the queen attacks any queen in the lower left diagonal position.
+*@param {array} arr: array of currently valid queen locations.
+*@param {int} row: location of next location to check.
+*@returns {boolean}: returns boolean value of true if the location passed false otherwise.
+*/
 function check_lower_diag(arr, row) {
+	//increment row to not check current position (can't attack itself).
 	row += 1;
+	//loop to move to the left until you hit the left side of the board.
 	for (var i = arr.length-1; i >= 0; i -=1) {
 		if (arr[i] === row) {return false;}
 		row += 1; 
@@ -48,6 +89,12 @@ function check_lower_diag(arr, row) {
 	return true;
 }
 
+/*
+*check_row() checks if the queen attacks any queen in the same row. Simple array check.
+*@param {array} arr: array of currently valid queen locations.
+*@param {int} row: location of next location to check.
+*@returns {boolean}: returns boolean value of true if the location passed false otherwise.
+*/
 function check_row(arr, row) {
 	return !arr.includes(row);
 }
--- javascript/NQueens_Init.js
+++ javascript/NQueens_Init.js
@@ -20,7 +20,7 @@ function init() {
 
 /**
 *init_title() is used upon document load to add the h1 header title
-*@param {node} container The container to hold the title element
+*@param {node} container: The container to hold the title element
 *@return nothing
 */
 function init_title(container) {
@@ -38,7 +38,7 @@ function init_title(container) {
 /**
 *init_taskbar() is used upon document load to add the taskbar
 *Taskbar contains both the board size slider and the solve button.
-*@param {node} container The container to hold the title element
+*@param {node} container: The container to hold the title element
 *@return nothing
 */
 function init_taskbar(container) {
@@ -57,7 +57,7 @@ function init_taskbar(container) {
 *init_slider() is used upon document load to add the slider
 *to the taskbar. Contains both slider and text div to display
 *value of the slider.
-*@param {node} container The container to hold the title element
+*@param {node} container: The container to hold the title element
 *@return nothing
 */
 function init_slider(container) {
@@ -77,6 +77,7 @@ function init_slider(container) {
 	slider.id = "size_slider";
 
 	//functionality to display the sliders value in the text element
+	//updates grid when slid
 	text.innerText = slider.value;
 	slider.oninput = function() {
 		text.innerText = this.value;
@@ -92,7 +93,7 @@ function init_slider(container) {
 *init_solveBtn() is used upon document load to add the solve button
 *to the taskbar. Solve button will start solving for the current
 *grid size once clicked.
-*@param {node} container The container to hold the title element
+*@param {node} container: The container to hold the title element
 *@return nothing
 */
 function init_solveBtn(container) {
@@ -121,7 +122,7 @@ function init_solveBtn(container) {
 /**
 *init_canvas() is used upon document load to add the canvas
 *to the page. Will draw blank board upon creation.
-*@param {node} container The container to hold the title element
+*@param {node} container: The container to hold the title element
 *@return nothing
 */
 function init_canvas(container) {
@@ -149,7 +150,7 @@ function init_canvas(container) {
 *init_stepCounter() is used upon document load to add the step counter
 *to the bottom of the element to track how many steps it takes the
 *algoritm to solve
-*@param {node} container The container to hold the title element
+*@param {node} container: The container to hold the title element
 *@return nothing
 */
 function init_stepCounter(container) {
